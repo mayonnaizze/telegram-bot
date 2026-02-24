6f22880eeb8f01442c46eb5cbf51b650705b0836
mayonnaizze <lpszagovor@gmail.com>
2026-02-24 20:21:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 import asyncio
 import random
 import os
@@ -151,4 +149,5 @@ async def handle_message(message: types.Message):
 ...     await dp.start_polling(bot)
 ... 
 ... if __name__ == "__main__":
+
 
